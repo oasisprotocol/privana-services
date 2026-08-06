3edf0fca823997d5fe70a3ac04b606770c4f6ef0
Matevž Jekovec <matevz@oasisprotocol.org>
2026-08-06 13:55:00 +0200

    docker: Don't deploy and upgrade contract on startup

--- docker/start.sh
+++ docker/start.sh
@@ -4,10 +4,6 @@ set -a
 
 source .env
 
-echo -n "Deriving OWNER_SECRET_KEY_ID from KMS..."
-SECRET_KEY=`curl -s  --json '{ "key_id": "OWNER_SECRET_KEY_ID", "kind": "secp256k1" }' --unix-socket /run/rofl-appd.sock http://localhost/rofl/v1/keys/generate | jq -r .key`
-echo "done."
-
 pushd solidity
   if [ "${ACCOUNTING_CHAIN_ID}" = "23293" ]; then
     NETWORK=sapphire-localnet
@@ -17,21 +13,11 @@ pushd solidity
     NETWORK=sapphire
   fi
 
-  LIQUIDITY_PROVIDER_ADDRESS=`SECRET_KEY=${LIQUIDITY_PROVIDER_SECRET_KEY} npx hardhat deployerAddress --network ${NETWORK}`
-  OWNER_ADDRESS=`npx hardhat deployerAddress --network ${NETWORK}`
-
-  echo "Deploying or upgrading privana-services contracts on NETWORK=${NETWORK}..."
-
-  OUT=`npx hardhat deploy --network ${NETWORK} --accountingaddress ${ACCOUNTING_CONTRACT_ADDRESS} --swapmanageraddress "${SWAP_MANAGER_CONTRACT_ADDRESS}" --lpaddress ${LIQUIDITY_PROVIDER_ADDRESS} --earnmanageraddress "${EARN_MANAGER_CONTRACT_ADDRESS}"`
-
-  EARN_MANAGER_CONTRACT_ADDRESS=`echo "$OUT" | sed -n 's/.*EarnManager proxy at: //p'`
-  SWAP_MANAGER_CONTRACT_ADDRESS=`echo "$OUT" | sed -n 's/.*SwapManager at: //p'`
-
-  echo -e "\nOWNER_ADDRESS=${OWNER_ADDRESS}, EARN_MANAGER_CONTRACT_ADDRESS=${EARN_MANAGER_CONTRACT_ADDRESS}, SWAP_MANAGER_CONTRACT_ADDRESS=${SWAP_MANAGER_CONTRACT_ADDRESS}"
+  LIQUIDITY_PROVIDER_ADDRESS=`SECRET_KEY=${LIQUIDITY_PROVIDER_SECRET_KEY} npx hardhat deployer:address --network ${NETWORK}`
 popd
 
-echo -n "Updating replica metadata for OWNER_ADDRESS..."
-curl  -X POST --json "{ \"OWNER_ADDRESS\": \"${OWNER_ADDRESS}\", \"EARN_MANAGER_CONTRACT_ADDRESS\": \"${EARN_MANAGER_CONTRACT_ADDRESS}\", \"SWAP_MANAGER_CONTRACT_ADDRESS\": \"${SWAP_MANAGER_CONTRACT_ADDRESS}\" }" --unix-socket /run/rofl-appd.sock http://localhost/rofl/v1/metadata
+echo -n "Updating replica metadata..."
+curl  -X PUT --json "{ \"EARN_MANAGER_CONTRACT_ADDRESS\": \"${EARN_MANAGER_CONTRACT_ADDRESS}\", \"SWAP_MANAGER_CONTRACT_ADDRESS\": \"${SWAP_MANAGER_CONTRACT_ADDRESS}\", \"LIQUIDITY_PROVIDER_ADDRESS\": \"${LIQUIDITY_PROVIDER_ADDRESS}\" }" --unix-socket /run/rofl-appd.sock http://localhost/rofl/v1/metadata
 echo "done."
 
 uv run uvicorn src.main:app --host ${API_HOST} --port ${API_PORT}
